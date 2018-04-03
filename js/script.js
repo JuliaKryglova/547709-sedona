c1b471a1200c7deb8dd5f1fba8999745d92236e9
JuliaKryglova <kryglovaj@gmail.com>
2018-04-03 19:47:23 +0300

    изменения 3.04 (4)

--- js/script.js
+++ js/script.js
@@ -6,18 +6,21 @@ var departure = popup.querySelector("[name=date-of-departure]");
 var people = popup.querySelector("[name=people");
 var form = popup.querySelector(".booking-form");
 
+
+popup.classList.add("form-show");
+
 button.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  popup.classList.add("form-show");
-  arrival.focus();
-  popup.classList.toggle("active");
+evt.preventDefault();
+popup.classList.add("form-show");
+arrival.focus();
+popup.classList.toggle("active");
 });
 
 form.addEventListener("submit", function(evt) {
-  if (!arrival.value || !departure.value || !people.value) {
-    evt.preventDefault();
-    popup.classList.remove("modal-error");
-    popup.offsetWidth = popup.offsetWidth;
-    popup.classList.add("modal-error");
-  }
+if (!arrival.value || !departure.value || !people.value) {
+evt.preventDefault();
+popup.classList.remove("modal-error");
+popup.offsetWidth = popup.offsetWidth;
+popup.classList.add("modal-error");
+}
 });
